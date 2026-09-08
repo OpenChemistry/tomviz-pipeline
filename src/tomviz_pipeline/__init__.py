e834a921f6ff169452dd68f396447df0ae58bc78
Alessandro Genova <alessandro.genova@kitware.com>
2026-09-08 19:06:42 -0400

    Bump version to 3.1.5
    Signed-off-by: Alessandro Genova <alessandro.genova@kitware.com>

--- src/tomviz_pipeline/__init__.py
+++ src/tomviz_pipeline/__init__.py
@@ -45,7 +45,7 @@ from .core import (  # noqa: F401
     set_port_data_serializer,
 )
 
-__version__ = '3.1.4'
+__version__ = '3.1.5'
 
 # Lazily resolved tomviz-layer exports: name -> (module, attribute).
 _LAZY = {
